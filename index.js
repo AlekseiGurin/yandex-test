3ae7b83625c28dce97882cf8877591b5e4f6e5ad
AlekseiGurin <dmb2009isr@bk.ru>
2025-01-16 16:14:05 +0700

    work with handlers

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 window.addEventListener("load", () => {
    console.log("hello");
+   const width1 = 1366;
+   const screenWidth = window.screen.width;
    const leftButton = document.getElementById("leftParticipantCardButton");
    const rightButton = document.getElementById("rightParticipantCardButton");
-   const counterLine = document.getElementById("participantСardСounter");
-   const slider = document.getElementById("slider");
+   const slider = document.querySelector(".carusel-slide");
+   const displayedCard = document.querySelector(".displayed-card");
+   const totalParticipantCard = document.querySelector(".total-participant-card");
    const cards = document.querySelectorAll(".partisipant-card");
+   let leftButtonISdisabled = true;
+   let numberOfCards = 3
+   if(screenWidth < width1) {
+      numberOfCards = 1
+   };
+   totalParticipantCard.textContent = `/${cards.length}`;
+   displayedCard.textContent = numberOfCards;
    let counter = 0;
-   const step = cards[0].clientWidth;
+   const step = cards[0].clientWidth * numberOfCards;
    console.log("step", step);
-   slider.style.translate = 'translateX(' + `${-step * counter}px)`;
-   rightButton.addEventListener('click', () => {
-    counter ++
-    slider.style.translate = 'translateX(' + `${-step * counter}px)`;
-   })
+   console.log("screenWidth", screenWidth);
+   if (!counter) {
+      leftButton.classList.add('disabled');
+   } 
+   const moveParticipantCardsToTheLeft = () => {
+      if (!leftButtonISdisabled) {
+         counter --
+         console.log("click leftButton")
+         leftButtonISdisabled = true;
+         if (screenWidth < width1) {
+            displayedCard.textContent = counter - numberOfCards;
+         } else {
+            displayedCard.textContent = numberOfCards;
+         };
+         console.log("click")
+         rightButton.classList = "participant-card-button";
+         leftButton.classList.add('disabled');
+         slider.style.transform = 'translateX(' + `${step * counter}px)`;
+      }
+   }
+   const moveParticipantCardsToTheRight = () => {
+      console.log("click rightButton")
+      if (leftButtonISdisabled) {
+         counter ++
+         leftButtonISdisabled = false;
+         if (screenWidth < width1) {
+            displayedCard.textContent = counter + numberOfCards;
+         } else {
+            displayedCard.textContent = numberOfCards + numberOfCards;
+         };
+         leftButton.classList = "participant-card-button";
+         rightButton.classList.add('disabled');
+         slider.style.transform = 'translateX(' + `${-step * counter}px)`;
+      }
+   }
+   rightButton.addEventListener('click', moveParticipantCardsToTheRight);
+   leftButton.addEventListener('click', moveParticipantCardsToTheLeft);
+
 });
