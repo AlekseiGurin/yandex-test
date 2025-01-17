8ece65511d026c6f430e16c2365d143bf93b6a44
AlekseiGurin <dmb2009isr@bk.ru>
2025-01-17 18:57:17 +0700

    work

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ window.addEventListener("load", () => {
    console.log("hello");
    const width1 = 1366;
    const screenWidth = window.screen.width;
-   const leftButton = document.getElementById("leftParticipantCardButton");
-   const rightButton = document.getElementById("rightParticipantCardButton");
+   const isDesctop = !(screenWidth < width1);
+   const leftButton = isDesctop ? document.getElementById("leftParticipantCardButton") : document.getElementById("leftMobileButton");
+   const rightButton = isDesctop ? document.getElementById("rightParticipantCardButton") : document.getElementById("rightMobileButton");
+   const displayedCard = isDesctop ? document.querySelector(".displayed-card") : document.querySelector(".displayed-mobile-card");
+   const totalParticipantCard = isDesctop ? document.querySelector(".total-participant-card") :document.querySelector(".total-participant-card-mobile");
+   console.log("totalParticipantCard", totalParticipantCard);
    const slider = document.querySelector(".carusel-slide");
-   const displayedCard = document.querySelector(".displayed-card");
-   const totalParticipantCard = document.querySelector(".total-participant-card");
    const cards = document.querySelectorAll(".partisipant-card");
-   let leftButtonISdisabled = true;
+   let leftButtonIsDisabled = true;
    let numberOfCards = 3
-   if(screenWidth < width1) {
+   if(!isDesctop) {
       numberOfCards = 1
    };
    totalParticipantCard.textContent = `/${cards.length}`;
@@ -18,42 +20,64 @@ window.addEventListener("load", () => {
    let counter = 0;
    const step = cards[0].clientWidth * numberOfCards;
    console.log("step", step);
+   console.log("counter", counter);
    console.log("screenWidth", screenWidth);
+   console.log("isDesctop", isDesctop);
+   
    if (!counter) {
       leftButton.classList.add('disabled');
    } 
    const moveParticipantCardsToTheLeft = () => {
-      if (!leftButtonISdisabled) {
-         counter --
-         console.log("click leftButton")
-         leftButtonISdisabled = true;
-         if (screenWidth < width1) {
-            displayedCard.textContent = counter - numberOfCards;
+      if (!leftButtonIsDisabled) {
+         if (!isDesctop) {
+            displayedCard.textContent = counter;
          } else {
             displayedCard.textContent = numberOfCards;
          };
+         counter --;
+         console.log("click leftButton");
+         console.log("counter", counter);
+         console.log("step", step);
+         if(counter === 0) {
+            leftButtonIsDisabled = true;
+            leftButton.classList.add('disabled');
+         }
          console.log("click")
-         rightButton.classList = "participant-card-button";
-         leftButton.classList.add('disabled');
-         slider.style.transform = 'translateX(' + `${step * counter}px)`;
-      }
-   }
+         rightButton.classList = "slider-button";
+         console.log("-step * counter + step", -step * counter + step)
+         slider.style.transform = 'translateX(' + `${-step * counter}px)`;
+      };
+   };
    const moveParticipantCardsToTheRight = () => {
       console.log("click rightButton")
-      if (leftButtonISdisabled) {
+      if (counter < cards.length -1) {
          counter ++
-         leftButtonISdisabled = false;
-         if (screenWidth < width1) {
-            displayedCard.textContent = counter + numberOfCards;
+         leftButtonIsDisabled = false;
+         if (!isDesctop) {
+            displayedCard.textContent = counter + 1;
          } else {
             displayedCard.textContent = numberOfCards + numberOfCards;
          };
-         leftButton.classList = "participant-card-button";
-         rightButton.classList.add('disabled');
+         if (isDesctop || !isDesctop && counter === cards.length - 1) {
+            rightButton.classList.add('disabled');
+         }
+         leftButton.classList = "slider-button";
+         console.log("-step * counter", -step * counter)
          slider.style.transform = 'translateX(' + `${-step * counter}px)`;
-      }
+      } 
+      console.log("counter", counter);
    }
    rightButton.addEventListener('click', moveParticipantCardsToTheRight);
    leftButton.addEventListener('click', moveParticipantCardsToTheLeft);
-
+   // setInterval(() => {
+   //    console.log("setTimeout")
+   //   if (leftButtonIsDisabled || screenWidth < width1 && counter < 6) {
+   //      moveParticipantCardsToTheRight();
+   //   } else {
+   //    if (screenWidth < width1) {
+         
+   //    }
+   //      moveParticipantCardsToTheLeft();
+   //   }
+   // }, 4000)
 });
